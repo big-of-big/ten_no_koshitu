4894c1ebb5464cb2a55eaeaf8ef31719c471d55f
lamp7600 <newlamp7600@gmail.com>
2020-10-09 22:14:37 +0900

    deviseのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users, skip: %i[sessions password]
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+    post 'signup', to: 'devise/registrations#create'
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'logout', to: 'devise/sessions#destroy'
+    get 'password', to: 'devise/passwords#edit'
+    patch 'password', to: 'devise/passwords#update'
+    delete 'retirement', to: 'devise/registrations#destroy'
+  end
   resources :users
 
   root to: 'welcome#index'
   get 'logs/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
