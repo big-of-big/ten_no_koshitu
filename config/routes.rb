866f04c4e51e99d6f6ebcf67295f93cdd06e221e
lamp7600 <newlamp7600@gmail.com>
2020-10-10 10:20:36 +0900

    パスワード変更のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: %i[sessions password]
+  root to: 'welcome#index'
+
+  devise_for :users, skip: %i[sessions registrations]
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
     post 'signup', to: 'devise/registrations#create'
     get 'login', to: 'devise/sessions#new'
+    # deviseが内部で使用するヘルパーメソッド
+    get 'login', to: 'devise/sessions#new', as: 'new_user_session'
     post 'login', to: 'devise/sessions#create'
     delete 'logout', to: 'devise/sessions#destroy'
-    get 'password', to: 'devise/passwords#edit'
-    patch 'password', to: 'devise/passwords#update'
     delete 'retirement', to: 'devise/registrations#destroy'
   end
-  resources :users
 
-  root to: 'welcome#index'
+  resources :users, except: %i[new create]
   get 'logs/index'
 end
