6a5ae03695a2c363d4bae89a5d5167e010924e40
lamp7600 <newlamp7600@gmail.com>
2020-10-11 22:17:01 +0900

    毎日夜12:30にログを取得する

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 
 set :job_template, "/bin/zsh -l -c ':job'"
+
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
-every 1.minute do
+every 1.day, at: "3:30 pm" do
   begin
-    rake "hello_world:hello"
+    rake "logs:private_room"
   rescue => e
     raise e
   end
 end
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
