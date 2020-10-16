5604f3ec43babfa806a39d146858f887d65850e8
lamp7600 <newlamp7600@gmail.com>
2020-10-16 16:58:36 +0900

    ログの表示に日付を追加

--- app/controllers/teams/logs_controller.rb
+++ app/controllers/teams/logs_controller.rb
@@ -2,8 +2,7 @@ class Teams::LogsController < ApplicationController
   def index
     logs = TeamLog.where(team_id: params[:id])
     @team_logs = logs.map do |log|
-      JSON.parse(log.content)
+      { name: log.name, content: JSON.parse(log.content) }
       end
-    @team_logs.flatten!
   end
 end
