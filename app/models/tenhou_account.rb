444741f0296825c31bb9b7c83dfb8bc5d4114b0f
lamp7600 <newlamp7600@gmail.com>
2020-10-24 11:37:32 +0900

    天鳳アカウント名にバリデーションを追加

--- app/models/tenhou_account.rb
+++ app/models/tenhou_account.rb
@@ -1,3 +1,14 @@
 class TenhouAccount < ApplicationRecord
   belongs_to :team
+  validates :name,
+    presence: true,
+    length: { maximum: 8 },
+    uniqueness: { scope: :team_id }
+  validate :correct_tenhou_account
+
+  def correct_tenhou_account
+    if [" ", "　", "(" ,")", "（" ,"）"].any? { |i| name.include?(i) }
+      errors.add(:name, "に使用できない文字が含まれています")
+    end
+  end
 end
