dfa870e61a83acdecb49e17a6f8abd4ae6fa1c29
lamp7600 <newlamp7600@gmail.com>
2020-10-29 14:48:50 +0900

    RailsからVueにデータを渡せるようにした

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,12 @@
 <template>
   <div id="app">
     <p>期間を選択</p>
-    <!-- <p>{{ message }}</p> -->
     <input type="date" v-model="start_date">
     <span>〜</span>
     <input type="date" v-model="end_date">
     <button @click="num += 1">決定</button>
-    <pre> {{ num }} </pre>
-    <pre> {{ start_date }} </pre>
-    <pre> {{ end_date }} </pre>
-    <pre> {{ name }} </pre>
     <pre> {{ $data }} </pre>
+    <pre> {{ tenhou_name }} </pre>
   </div>
 </template>
 
@@ -18,18 +14,13 @@
 export default {
   data: function () {
     return {
-      // message: "Hello Vue!"
       num: 0,
       start_date: '',
       end_date: '',
-      name: ''
     }
   },
   props: {
     tenhou_name: { type: String }
-  },
-  mounted: function (){
-    this.name = this.tenhou_name
   }
 }
 </script>
