41a1937613fb2198e3fff8774bb46a4ccfdfbb9c
lamp7600 <newlamp7600@gmail.com>
2020-10-29 17:34:26 +0900

    繰り返し処理を記述(WIP)

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -7,7 +7,12 @@
     <button @click="display()">決定</button>
     <pre> {{ $data }} </pre>
     <pre> {{ tenhou_name }} </pre>
-    <pre> {{ one_month_games }} </pre>
+    <ul>
+      <li v-for="one_month_game in JSON.parse(one_month_games)" :key="one_month_game.name">
+        {{ set(one_month_game.four_games) }}
+        {{ one_month_game }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -17,17 +22,24 @@ export default {
     return {
       num: 0,
       start_date: '',
-      end_date: ''
+      end_date: '',
+      four_games_scores: '',
+      four_games_rankings: '',
+      three_games_scores: '',
+      three_games_rankings: ''
     }
   },
   props: {
-    tenhou_name: { type: String, },
-    one_month_games: {type: Object}
+    tenhou_name: { type: String },
+    one_month_games: {type: String}
   },
   methods: {
     display: function () {
       alert(this.start_date)
       alert(this.end_date)
+    },
+    set: function(value) {
+      this.four_games_scores = value
     }
   }
 }
