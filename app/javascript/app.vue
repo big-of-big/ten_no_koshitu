2ec4a683ec1a8fc7341f03c351cdc55c43d5a0ff
lamp7600 <newlamp7600@gmail.com>
2020-10-30 18:49:31 +0900

    v-modelで任意期間のログを取得できるようにした

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,15 +4,25 @@
     <input type="date" v-model="start_date">
     <span>〜</span>
     <input type="date" v-model="end_date">
-    <button @click="display()">決定</button>
+    <p>4人打ち</p>
+    <p>3人打ち</p>
     <pre> {{ $data }} </pre>
-    <pre> {{ tenhou_name }} </pre>
-    <ul>
-      <li v-for="one_month_game in JSON.parse(one_month_games)" :key="one_month_game.name">
-        {{ set(one_month_game.four_games) }}
-        {{ one_month_game }}
-      </li>
-    </ul>
+    <!-- <pre> {{ tenhou_name }} </pre> -->
+    <!-- {{ selected_four_games}} -->
+    {{ selected_three_games}}
+    <!-- <ul> -->
+      <!-- <li v&#45;for="four_game in JSON.parse(four_games)" > -->
+        <!-- {{ set(one_month_game.four_games) }} -->
+        <!-- {{ four_game }} -->
+        <!-- {{ set_four_games(four_game) }} -->
+      <!-- </li> -->
+    <!-- </ul> -->
+    <!-- <ul> -->
+    <!--   <li v&#45;for="three_game in three_games" > -->
+    <!--     <!&#45;&#45; {{ set(one_month_game.four_games) }} &#45;&#45;> -->
+    <!--     <!&#45;&#45; {{ three_game }} &#45;&#45;> -->
+    <!--   </li> -->
+    <!-- </ul> -->
   </div>
 </template>
 
@@ -20,7 +30,6 @@
 export default {
   data: function () {
     return {
-      num: 0,
       start_date: '',
       end_date: '',
       four_games_scores: '',
@@ -31,15 +40,31 @@ export default {
   },
   props: {
     tenhou_name: { type: String },
-    one_month_games: {type: String}
+    one_month_games: {type: String},
+    three_games_string: {type: String},
+    four_games: {type: String}
   },
   methods: {
-    display: function () {
-      alert(this.start_date)
-      alert(this.end_date)
+  },
+  computed: {
+    three_games: function() {
+      return JSON.parse(this.three_games_string)
     },
-    set: function(value) {
-      this.four_games_scores = value
+    selected_three_games: function () {
+      // 開始か終了の期間が変更されるとこの処理が実行される
+
+      const start = new Date(this.start_date)
+      const end = new Date(this.end_date)
+      const three_games = this.three_games
+      let ary = []
+      for(const three_game of three_games) {
+        let date = new Date(three_game.date)
+        if(date >= start && date <= end){
+          ary.push(three_game)
+        }
+      }
+      // alert(end)
+      return ary
     }
   }
 }
