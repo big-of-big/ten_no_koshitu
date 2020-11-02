d5b131056f829d50c3d211758c57c7d4a4ca2ec3
lamp7600 <newlamp7600@gmail.com>
2020-11-02 12:47:16 +0900

    全期間にチェックが入るとカレンダーの期間が消えて、カレンダーの期間が選択されると全期間のチェックを消す

--- app/javascript/packs/table.js
+++ app/javascript/packs/table.js
@@ -9,6 +9,42 @@ document.addEventListener('turbolinks:load', () => {
       start: '',
       end: ''
     },
-    components: { Hoge }
+    components: { Hoge },
+    computed: {
+      changeAll: {
+        get: function () {
+          return this.all
+        },
+        set: function (boolean) {
+          this.all = boolean
+          if (this.all === true) {
+            this.start = ''
+            this.end = ''
+          }
+        },
+      },
+      changeStart: {
+        get: function () {
+          return this.start
+        },
+        set: function (date) {
+          this.start = date
+          if (!(this.start === '')) {
+            this.all = false
+          }
+        }
+      },
+      changeEnd: {
+        get: function () {
+          return this.end
+        },
+        set: function (date) {
+          this.end = date
+          if (!(this.end === '')) {
+            this.all = false
+          }
+        }
+      }
+    }
   })
 })
