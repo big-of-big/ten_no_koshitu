5b3d5c2a9f10a836ade1b05c83f53d9619f8c965
lamp7600 <newlamp7600@gmail.com>
2020-11-04 17:00:03 +0900

    JSファイルをリネーム

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -1,12 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
-import App from '../app.vue'
+import Result from '../result.vue'
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#results',
-    data: {
-      message: "",
-    },
-    components: { App }
+    components: { Result }
   })
 })
--- app/javascript/packs/table.js
+++ app/javascript/packs/table.js
@@ -1,5 +1,5 @@
 import Vue from 'vue/dist/vue.esm'
-import User from '../table.vue'
+import User from '../user.vue'
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
