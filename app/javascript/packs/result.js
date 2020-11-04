1de1bfba9de20215d239abf87560f7ef759320e9
lamp7600 <newlamp7600@gmail.com>
2020-11-04 17:22:03 +0900

    vueをターボリンクを使う設定にする

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -1,5 +1,8 @@
+import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import Result from '../result.vue'
+
+Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
--- app/javascript/packs/table.js
+++ app/javascript/packs/table.js
@@ -1,5 +1,8 @@
+import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import User from '../user.vue'
+
+Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
