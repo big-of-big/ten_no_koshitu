c8d3234d373df9fbb01744a618ec83550a568c30
lamp7600 <newlamp7600@gmail.com>
2020-11-06 15:19:45 +0900

    天鳳名のバリデーションを追加

--- app/models/tenhou_account.rb
+++ app/models/tenhou_account.rb
@@ -7,7 +7,7 @@ class TenhouAccount < ApplicationRecord
   validate :correct_tenhou_account
 
   def correct_tenhou_account
-    if [" ", "　", "(" ,")", "（" ,"）"].any? { |i| name.include?(i) }
+    if [" ", "　"].any? { |i| name.include?(i) }
       errors.add(:name, "に使用できない文字が含まれています")
     end
   end
