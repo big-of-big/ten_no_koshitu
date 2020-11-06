358a255f847cd7141f23f9d8082ad73bebc8d4e8
lamp7600 <newlamp7600@gmail.com>
2020-11-06 22:05:20 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20201103054543_remove_user_id_from_tenhou_accounts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromTenhouAccounts < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :tenhou_accounts, :user_id, :integer
-  end
-end
--- db/migrate/20201106054022_remove_log_from_teams.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLogFromTeams < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :teams, :log, :text
-  end
-end
