c03faac2343e00c0997fe83c2ad2fc47da021cf1
lamp7600 <newlamp7600@gmail.com>
2020-11-07 18:07:17 +0900

    seedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,28 @@ def private_room_log(date)
   Zlib::GzipReader.open(tempfile) {|gz|
     log = gz.read
   }
-  {name: date.strftime("%Y/%m/%d"), content: log}
+  { name: date.strftime("%Y/%m/%d"), content: log }
 end
 
+def user_seed(name)
+  {
+    name: name,
+    email: "#{name}@example.com",
+    password: "password",
+    team_id: 1
+  }
+end
+
+names = ["こむぎ","夜叉丸","グランドクロス","麻雀たろう","v-bind"]
+
 if Rails.env == "development"
-  Log.create!(private_room_log(Time.new(2020,10,10)))
-  Log.create!(private_room_log(Time.new(2020,10,15)))
+  Team.create!(id: 1, name: "バックエンド麻雀部", join_key: 34323)
+  User.create!(user_seed("komugi"))
+  User.create!(user_seed("yasyamaru"))
+  names.each do |name|
+    TenhouAccount.create!(name: name, team_id: 1)
+  end
+
+  # Log.create!(private_room_log(Time.new(2020,10,10)))
+  # Log.create!(private_room_log(Time.new(2020,10,15)))
 end
