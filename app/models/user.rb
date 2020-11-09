b66ff8a941c2ae5f4c288f6bebd809f6a65501ed
lamp7600 <newlamp7600@gmail.com>
2020-11-09 22:55:08 +0900

    ログアウトできないバグを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true
