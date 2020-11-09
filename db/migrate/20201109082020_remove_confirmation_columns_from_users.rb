24e64ce9784ed0b7f85ad8d4578458dd8ecf53b9
lamp7600 <newlamp7600@gmail.com>
2020-11-09 17:04:23 +0900

    不要なdevise関係のカラムを削除

---
+++ db/migrate/20201109082020_remove_confirmation_columns_from_users.rb
@@ -0,0 +1,10 @@
+class RemoveConfirmationColumnsFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :confirmation_token, :string
+    remove_column :users, :confirmed_at, :datetime
+    remove_column :users, :confirmation_sent_at, :datetime
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+  end
+end
