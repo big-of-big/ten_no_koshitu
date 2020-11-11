6836ec1009abc717f7b8f3a88a02b43e28e8f056
lamp7600 <newlamp7600@gmail.com>
2020-11-11 14:54:53 +0900

    rack_attackの値を修正

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,14 +3,14 @@ class Rack::Attack
     req.ip # unless req.path.start_with?('/assets')
   end
 
-  # 例: 同一IPアドレスからの/loginへのPOSTリクエストを20秒あたり5回までに制限
-  throttle('login/ip', :limit => 5, :period => 20.seconds) do |req|
+  # 例: 同一IPアドレスからの/loginへのPOSTリクエストを10秒あたり5回までに制限
+  throttle('login/ip', :limit => 5, :period => 10.seconds) do |req|
     if req.path == '/login' && req.post?
       req.ip
     end
   end
 
-  throttle('signup/ip', :limit => 5, :period => 20.hours) do |req|
+  throttle('signup/ip', :limit => 20, :period => 20.hours) do |req|
     if req.path == '/signup' && req.post?
       req.ip
     end
