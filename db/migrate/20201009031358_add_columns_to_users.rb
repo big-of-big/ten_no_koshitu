aeccbdca2a044a0abcc3860225a9cee4174f2ba8
lamp7600 <newlamp7600@gmail.com>
2020-11-11 12:19:42 +0900

    編集してしまった過去のマイグレーションファイルを元に戻した

--- db/migrate/20201009031358_add_columns_to_users.rb
+++ db/migrate/20201009031358_add_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string, null: false
     add_column :users, :tenhou_account, :string
+    add_column :users, :image, :string
     add_column :users, :team_id, :integer
-    add_index :users, :name, unique: true
   end
 end
