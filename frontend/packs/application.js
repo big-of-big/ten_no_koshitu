7e0a627cf2a2b7e3f90767ccb948f90b0d598edc
lamp7600 <newlamp7600@gmail.com>
2020-11-15 16:03:34 +0900

    ファビコンを配置

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -9,6 +9,7 @@ require('channels')
 
 import "bootstrap"
 import "../stylesheets/application"
+import "../images/favicon.ico"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
