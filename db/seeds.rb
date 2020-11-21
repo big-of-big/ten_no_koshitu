cfb62afc9c8da27aa6dac874023c97df4615ca13
lamp7600 <newlamp7600@gmail.com>
2020-11-21 17:27:28 +0900

    seedを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ if Rails.env == "development"
   names.each do |name|
     TenhouAccount.create!(name: name, team_id: 1)
   end
-  Log.create!(Log.private_room_log(date: Time.new(2020, 11, 7)))
-  Log.create!(Log.private_room_log(date: Time.new(2020, 11, 8)))
+
+  start = Date.new(2020, 5)
+  last = Date.new(2020, 11)
+  (start..last).each do |date|
+  TeamLog.create!(
+     name: date.strftime("%Y/%m/%d"),
+     team_id: 1,
+     content: "[\"L1412 | 18:25 | 三般東喰赤－ | 夜叉丸(+46.0) こむぎ(-7.0) v-bind(-39.0)\", \"L1412 | 18:33 | 四般東喰赤－ | 夜叉丸(+40.0) v-bind(+5.0) こむぎ(-15.0) グランドクロス(-30.0)\", \"L1412 | 18:44 | 三般東喰赤－ | 夜叉丸(+69.0) こむぎ(-18.0) グランドクロス(-51.0)\"]"
+  )
+  end
 end
