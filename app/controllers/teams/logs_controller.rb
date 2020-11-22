7512d586ab52a5955654bc8894aaf89c4ab20646
lamp7600 <newlamp7600@gmail.com>
2020-11-22 10:46:45 +0900

    チームログページのデザインを作成

--- app/controllers/teams/logs_controller.rb
+++ app/controllers/teams/logs_controller.rb
@@ -1,6 +1,6 @@
 class Teams::LogsController < ApplicationController
   def index
-    logs = TeamLog.where(team_id: params[:id])
+    logs = TeamLog.where(team_id: params[:id]).order(created_at: "DESC")
     @team_logs = logs.map do |log|
         { name: log.name, content: JSON.parse(log.content) }
       end
