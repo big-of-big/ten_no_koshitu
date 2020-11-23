01af6e7ff4f61fb8bcef09c19ec1b2a840e90331
lamp7600 <newlamp7600@gmail.com>
2020-11-23 12:38:58 +0900

    自分のチーム以外のログを見えないようにする

--- app/controllers/teams/logs_controller.rb
+++ app/controllers/teams/logs_controller.rb
@@ -1,6 +1,6 @@
 class Teams::LogsController < ApplicationController
   def index
-    @logs = TeamLog.where(team_id: params[:id])
+    @logs = TeamLog.where(team_id: current_user.team.id)
                   .order(created_at: "DESC")
                   .page(params[:page])
                   .per(10)
