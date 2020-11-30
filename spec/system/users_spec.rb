232333a4b46d85a89c11c37385ea432437fb2d17
lamp7600 <newlamp7600@gmail.com>
2020-11-30 11:49:30 +0900

    ログイン周りのテストを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,21 +1,73 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Users', type: :system do
-  # before do
-  #   @user = User.create!(name: 'komugi', password: "password")
-  # end
-
+RSpec.describe "Users", type: :system do
   it "ユーザーが新規登録できる" do
     visit signup_path
 
     fill_in "ユーザー名(ログインに使用します)", with: "komugi"
     fill_in "パスワード(6文字以上)", with: "password"
     fill_in "パスワード(確認)", with: "password"
-
-    # 更新実行
     click_button "ユーザー登録"
 
-    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
     expect(page).to have_content "アカウント登録が完了しました。"
+  end
+
+  it "同じ名前のユーザーは登録できない" do
+    User.create!(name: "komugi", password: "password")
+
+    visit signup_path
+
+    fill_in "ユーザー名(ログインに使用します)", with: "komugi"
+    fill_in "パスワード(6文字以上)", with: "password"
+    fill_in "パスワード(確認)", with: "password"
+    click_button "ユーザー登録"
+
+    expect(page).to have_content "ユーザー名はすでに存在します"
+  end
+
+  it "パスワードは6文字以下だと登録できない" do
+    visit signup_path
+
+    fill_in "ユーザー名(ログインに使用します)", with: "komugi"
+    fill_in "パスワード(6文字以上)", with: "pass"
+    fill_in "パスワード(確認)", with: "pass"
+    click_button "ユーザー登録"
+
+    expect(page).to have_content "パスワードは6文字以上で入力してください"
+  end
+
+  it "パスワードが違うと登録できない" do
+    visit signup_path
+
+    fill_in "ユーザー名(ログインに使用します)", with: "komugi"
+    fill_in "パスワード(6文字以上)", with: "password"
+    fill_in "パスワード(確認)", with: "hogehoge"
+    click_button "ユーザー登録"
+
+    expect(page).to have_content "パスワード(確認)とパスワードの入力が一致しません"
+  end
+
+  it "正しい情報でログインできる" do
+    User.create!(name: "komugi", password: "password")
+
+    visit login_path
+
+    fill_in "ユーザー名", with: "komugi"
+    fill_in "パスワード", with: "password"
+    click_button "ログイン"
+
+    expect(page).to have_content "ログインしました。"
+  end
+
+  it "間違った情報ではログインできる" do
+    User.create!(name: "komugi", password: "password")
+
+    visit login_path
+
+    fill_in "ユーザー名", with: "komugi"
+    fill_in "パスワード", with: "incorrect_password"
+    click_button "ログイン"
+
+    expect(page).to have_content "ユーザー名またはパスワードが違います。"
   end
 end
