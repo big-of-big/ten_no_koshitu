57278c30f38cfbc01536d5af9c5d6e07ab6f77d4
lamp7600 <newlamp7600@gmail.com>
2020-11-30 15:36:44 +0900

    testディレクトリを削除

--- test/controllers/teams/logs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Teams::LogsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get teams_logs_index_url
-    assert_response :success
-  end
-end
