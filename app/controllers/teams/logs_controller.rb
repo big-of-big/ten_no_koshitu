8012c9d7341b16b2c0e8fe75c74b88f34e53c935
lamp7600 <newlamp7600@gmail.com>
2020-12-08 16:17:14 +0900

    認証チェックを追加

--- app/controllers/teams/logs_controller.rb
+++ app/controllers/teams/logs_controller.rb
@@ -1,4 +1,6 @@
 class Teams::LogsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @logs = TeamLog.where(team_id: current_user.team.id)
                   .order(created_at: "DESC")
