85ea0632f91f231da22b12611cc3bccd2c72ea18
lamp7600 <newlamp7600@gmail.com>
2020-12-09 16:16:19 +0900

    メソッド名を変更

--- app/models/one_game.rb
+++ app/models/one_game.rb
@@ -4,11 +4,11 @@ class OneGame
   def initialize(name, one_game_log)
     @name = name
     @one_game_log = one_game_log
-    @type = set_type(one_game_log)
+    @type = game_type_of(one_game_log)
     @date = name.to_date
   end
 
-  def set_type(one_game_log)
+  def game_type_of(one_game_log)
     ary = one_game_log.split(" ")
 
     if ary.count == 10 ## 4人打ち
